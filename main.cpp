0be0426c9843da4ee1fbaf515e3e40a806013243
a_german06 <a.german.main@gmail.com>
2024-11-27 02:29:48 +0500

    Создание и заполнение массива

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
+
+    int *Array {new int[10]};
+    for (int i=0; i<10; i++){
+        Array[i] = rand()%21-10;
+    }
 
     return 0;
 }
